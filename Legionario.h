e1e4a111c95cd7f824b1b7a9bbe4e02597674cc6
Luan Oliveira <luanoliveira35@hotmail.com>
2015-05-26 20:20:42 -0300

    Criado Arquivo .h

---
+++ Legionario.h
@@ -0,0 +1,18 @@
+#include <string>
+using std::string;
+ 
+class Legionario{
+	public:
+	Legionario();
+	void gritar();
+	void atacar();
+	void defender();
+	
+	
+	private:
+	
+	int forca;
+	int velAtk;
+	static legUnidades;
+	
+};
